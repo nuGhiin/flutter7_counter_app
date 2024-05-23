0da7a68c6684b6cc037fc22752fbadf4a2edf4c9
nuGhinTanjid <tanjidit2@gmail.com>
2024-05-23 17:38:20 +0600

    init

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'appName',
+      home: HomeScreen(),
+    );
+  }
+}
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Text('Hi'),
+      ),
+    );
+  }
+}
+
+
+
+
+
